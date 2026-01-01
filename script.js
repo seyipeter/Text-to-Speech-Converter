beaaf94c81dbc9670df9a792741d685708c81343
Oluwafemi Peter <105382129+seyipeter@users.noreply.github.com>
2026-01-02 00:03:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const synth = window.speechSynthesis;
+
+const triggerButton = document.querySelector('button');
+const inputText = document.querySelector('textarea');
+
+const voiceSelect = document.querySelector('select');
+
+let voices = [];
+
+function populateVoiceList() {
+
+    voices = synth.getVoices();
+
+    for (const voice of voices) {
+
+        const option = document.createElement('option');
+        option.textContent = `${voice.name} (${voice.lang})`;
+
+        if (voice.default) {
+            option.textContent += " — DEFAULT";
+        }
+
+        option.setAttribute("data-lang", voice.lang);
+        option.setAttribute("data-name", voice.name);
+        voiceSelect.appendChild(option);
+
+    }
+}
+
+
+if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = populateVoiceList;
+}
+populateVoiceList();
+
+function preventingIntSubmit(event) {
+    event.preventDefault();
+
+    const utterThis = new SpeechSynthesisUtterance(inputText.value);
+
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+    for (const voice of voices) {
+        if (voice.name === selectedOption) {
+            utterThis.voice = voice;
+        }
+    }
+
+    synth.speak(utterThis);
+
+    inputText.blur();
+};
+triggerButton.addEventListener('click', preventingIntSubmit);
+
+
+
+
+
